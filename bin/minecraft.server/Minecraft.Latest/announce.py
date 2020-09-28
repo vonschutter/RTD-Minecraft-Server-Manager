e96b4f7540bbd886b263cfbbce8bf9d3602890c2
The Untouchables <34268921+vonschutter@users.noreply.github.com>
2020-09-28 21:29:19 +0200

    Update announce.py

--- bin/minecraft.server/Minecraft.Latest/announce.py
+++ bin/minecraft.server/Minecraft.Latest/announce.py
@@ -1,31 +1,31 @@
 #!/bin/python
-#::             RTD System System Managment Script
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#:: Author:     Stephan S. 
-#:: Version 1.00
-#::
-#::
-#:: Purpose: The purpose of the script is to:
-#::          1 - Broadcast service availability to the local network 
-#::          
-#::  
-#::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#              RTD System System Managment Script
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :: Author:     Stephan S. 
+# :: Version 1.00
+# ::
+# ::
+# :: Purpose: The purpose of the script is to:
+# ::          1 - Broadcast service availability to the local network 
+# ::          
+# ::  
+# ::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-#::::::::::::::                                          ::::::::::::::::::::::
-#::::::::::::::          Script Executive                ::::::::::::::::::::::
-#::::::::::::::                                          ::::::::::::::::::::::
-#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# :::::::::::::                                          ::::::::::::::::::::::
+# :::::::::::::          Script Executive                ::::::::::::::::::::::
+# :::::::::::::                                          ::::::::::::::::::::::
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 import socket
 import time
  
 servers = [
-	["Local Network - Minecraft Server", 25565],
+    ["Local Network - Minecraft Server", 25565],
 ]
 
 BROADCAST_IP = "255.255.255.255"
@@ -34,10 +34,10 @@ BROADCAST_PORT = 12000
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-print "Broadcasting Minecraft servers to LAN"
+print("Broadcasting Minecraft servers to LAN")
 
-while 1:
-        for server in servers:
-	        msg = "[MOTD]%s[/MOTD][AD]%d[/AD]" % (server[0], server[1])
-	        sock.sendto(msg, (BROADCAST_IP, BROADCAST_PORT))
-time.sleep(1.5)
+while True:
+    for server in servers:
+        msg = "[MOTD]%s[/MOTD][AD]%d[/AD]" % (server[0], server[1])
+        sock.sendto(msg, (BROADCAST_IP, BROADCAST_PORT))
+        time.sleep(1)
